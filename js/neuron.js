7fad5ca8560906a47ace96d510839be5f922493f
Ibrahim <ibrahim.97@hotmail.de>
2022-07-15 19:27:27 +0200

    Snap SVG extra changes

--- js/neuron.js
+++ js/neuron.js
@@ -1,30 +1,36 @@
 class neuron{
 
-  constructor(context, x, y) {
+  constructor(snap, e, x, y) {
     this.x = x;
     this.y = y;
-    this.context = context;
+    this.event = e;
+    this.snap = snap;
+    this.circle;
     this.hook = false;
     this.pushed = false;
     this.velocityX = 0.0;
     this.velocityY = 0.0;
-    this.diameter = 50;
+    this.radius = 20;
     this.diffLength = 0;
     this.normVX = 0;
     this.normVY = 0;
     this.diffX = 0;
     this.diffY = 0;
+
+    this.circle = this.snap.circle(this.x, this.y, this.radius);
+    this.circle.drag(move);
+
+    var move = function() {
+      var dx = this.x - e.clientX;
+      var dy = this.y - e.clientY;
+
+      this.x = this.x + dx;
+      this.y = this.y + dy;
+    }
   }
 
-  move() {
-    
-  }
+  
 
   display() {
-    this.context.beginPath();
-    this.context.lineWidth = 5;
-    this.context.arc(this.x, this.y, this.diameter, 0, Math.PI * 2);
-    this.context.stroke();
-    this.context.closePath();
   }
 }
