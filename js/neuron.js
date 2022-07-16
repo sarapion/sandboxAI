57f13b7ecf479738f83a8c2c76e5e192ea225820
Ibrahim <ibrahim.97@hotmail.de>
2022-07-16 04:29:25 +0200

    Starting with more efficiency in dragging

--- js/neuron.js
+++ js/neuron.js
@@ -17,14 +17,21 @@ class neuron{
     this.diffY = 0;
 
     this.circle = this.snap.circle(x, y, this.radius);
+   //this.circle.drag();
   }
 
-  move(x,y){
-    var dx = x - this.circle.getBBox().cx;
-    var dy = y - this.circle.getBBox().cy;
-    
-    
-    this.circle.attr({cx: this.circle.getBBox().cx - dx/6, cy: this.circle.getBBox().cy - dy/6});
+  move(x, y, mousedown){
+    var hover = (Math.sqrt(Math.pow(x - this.circle.getBBox().cx, 2) + Math.pow(y - this.circle.getBBox().cy, 2)) <= this.radius);
+    //console.log(hover);
+    //var hover = (Math.sqrt(Math.pow(x - this.circle.getBBox().cx, 2) + Math.pow(y - this.circle.getBBox().cy, 2)) <= this.radius);
+    //console.log(hover);
+    if(mousedown && (hover || this.hook || (this.diffX + this.diffY) !== 0)){
+      this.hook = true;
+      this.diffX  = (x - this.circle.getBBox().cx) / 5;
+      this.diffY = (y - this.circle.getBBox().cy) / 5;
+      console.log("test");
+      this.circle.attr({cx: this.circle.getBBox().cx + this.diffX, cy: this.circle.getBBox().cy + this.diffY});
+    }
   }
 
   display() {
