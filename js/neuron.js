0d763708e2e090c5e2ab537960031043483bfd61
Ibrahim <ibrahim.97@hotmail.de>
2022-07-18 03:46:41 +0200

    Back to HTML5 Canvas + Disabled Zoom

--- js/neuron.js
+++ js/neuron.js
@@ -1,10 +1,10 @@
 class neuron{
 
-  constructor(snap, x, y) {
-    this.snap = snap;
+  constructor(context, x, y) {
+    this.ctx = context;
     this.circle;
-    this.x = 0;
-    this.y = 0;
+    this.x = x;
+    this.y = y;
     this.hook = false;
     this.pushed = false;
     this.velocityX = 0.0;
@@ -16,24 +16,16 @@ class neuron{
     this.diffX = 0;
     this.diffY = 0;
 
-    this.circle = this.snap.circle(x, y, this.radius);
-   //this.circle.drag();
   }
 
   move(x, y, mousedown){
-    var hover = (Math.sqrt(Math.pow(x - this.circle.getBBox().cx, 2) + Math.pow(y - this.circle.getBBox().cy, 2)) <= this.radius);
-    //console.log(hover);
-    //var hover = (Math.sqrt(Math.pow(x - this.circle.getBBox().cx, 2) + Math.pow(y - this.circle.getBBox().cy, 2)) <= this.radius);
-    //console.log(hover);
-    if(mousedown && (hover || this.hook || (this.diffX + this.diffY) !== 0)){
-      this.hook = true;
-      this.diffX  = (x - this.circle.getBBox().cx) / 5;
-      this.diffY = (y - this.circle.getBBox().cy) / 5;
-      console.log("test");
-      this.circle.attr({cx: this.circle.getBBox().cx + this.diffX, cy: this.circle.getBBox().cy + this.diffY});
-    }
   }
 
-  display() {
+  draw() {
+    this.ctx.fillStyle = "blue";
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    this.ctx.closePath();
+    this.ctx.fill();
   }
 }
